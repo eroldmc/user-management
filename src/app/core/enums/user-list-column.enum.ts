b42eadccb837d6d62769b09c2a94c6b5df3e88a3
Demircan Erol <demircan.erol@outlook.com>
2025-03-12 13:47:46 +0100

    feat(user): Ajout des enums pour les types de données

---
+++ src/app/core/enums/user-list-column.enum.ts
@@ -0,0 +1,13 @@
+export enum UserListColumn {
+    ID = 'id',
+    UUID = 'uuid',
+    AVATAR = 'avatar',
+    FIRSTNAME = 'firstname',
+    LASTNAME = 'lastname',
+    FULLNAME = 'fullname',
+    EMAIL = 'email',
+    PHONE = 'phone',
+    ADDRESS = 'address',
+    CITY = 'city',
+    COUNTRY = 'country'
+}
